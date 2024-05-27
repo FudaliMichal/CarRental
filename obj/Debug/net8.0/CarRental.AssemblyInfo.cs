0da2defd9dee7008d553f8a81d01425c2d0ca0a9
Michal <micharegister@gmail.com>
2024-05-27 02:01:35 +0200

    logownaie kodpracownuczy

--- obj/Debug/net8.0/CarRental.AssemblyInfo.cs
+++ obj/Debug/net8.0/CarRental.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d24d8a2c18deb4342c94ecd836ec7aa94c59ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee4aeec798d6156988a112523c8952059ab1a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
